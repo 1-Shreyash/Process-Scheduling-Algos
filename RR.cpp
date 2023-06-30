65c42644b4df37acedb0164f0e909bac72adb8d8
Shreyash Santosh Shahu <sahushreyash48@gmail.com>
2023-06-30 19:38:07 +0530

    Add files via upload

---
+++ RR.cpp
@@ -0,0 +1,165 @@
+#include <stdio.h> //for sjf
+#include <stdlib.h>
+#include <set>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+struct process
+{
+    int bt, at, id;
+};
+struct gantt{
+    int prid, toa, endt, d;//time of arrival
+};
+int findMinAt(struct process pr[], set<int> remaining)
+{
+    int atMin = 100;
+    for (int i : remaining)
+    {
+        if (atMin > pr[i].at)
+            atMin = pr[i].at;
+    }
+    // cout<<"hs"<<endl;
+    return atMin;
+}
+void update(set<int> &remaining, queue<int> &arrived, struct process pr[], int t){
+    if (remaining.empty() == false)
+        {
+            vector<int> temp;
+            for (int i : remaining)
+            {
+                // cout << "me" << endl;
+                if (pr[i].at <= t)
+                {
+                    arrived.push(i);
+                    temp.push_back(i);
+                    // cout << "\t arrived:" << pr2[i].id << endl;
+                }
+            }
+            for (int i : temp)
+            {
+                remaining.erase(i);
+            }
+        }
+}
+int main()
+{
+    int pn = 5;
+    struct process pr[10];
+    struct process pr2[10];
+    vector<struct gantt> g;
+    set<int> remaining;
+    // for (int i = 0; i < 10; i++)
+    //     remaining.insert(i);
+    queue<int> arrived;
+    set<int> done;
+    rand();
+    for (int i = 0; i < 10; i++)
+    {
+        int a;
+        while ((a = rand() % 20) == 0);
+        pr[i].bt = a;
+        pr[i].at = rand() % 10;
+        pr[i].id = i;
+
+        pr2[i] = pr[i];
+    }
+    cout<<"<-------------ROUND ROBIN ALGO------------->\n\n";
+    printf("Process  Burst Time  Arrival Time\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("P%d\t  %d \t      %d \n", pr[i].id, pr[i].bt, pr[i].at);
+        // printf("hes\n");
+    }
+
+    // sorting the processes with respect to at
+    struct process temp;
+    for (int i = 1; i < 10; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (pr2[i].at < pr2[j].at)
+            {
+                temp = pr2[i];
+                pr2[i] = pr2[j];
+                pr2[j] = temp;
+            }
+            if(pr2[i].at == pr2[j].at){
+                if(pr2[i].id < pr2[j].id){
+                    temp = pr2[i];
+                    pr2[i] = pr2[j];
+                    pr2[j] = temp;
+                }
+            }
+        }
+    }
+    for(struct process p: pr2)
+        remaining.insert(p.id);
+    int t;
+    int j;
+    // function starts
+
+    while (arrived.empty() == false || remaining.empty() == false)
+    {
+        // cout << "hello" << endl;
+        if (arrived.empty() == true && remaining.empty() == false)
+            t = findMinAt(pr2, remaining);
+        // cout << "T : " << t << endl;
+        update(remaining, arrived, pr2, t);
+        if (arrived.empty() == false)
+        {
+            j = arrived.front();
+
+            struct gantt g1;
+            g1.prid = pr2[j].id;
+            g1.toa = t;
+            g1.d = 1;
+            arrived.pop();
+            if(pr2[j].bt>pn){
+                pr2[j].bt -= pn;
+                t += pn;
+                update(remaining, arrived, pr2, t);
+                arrived.push(j);
+                g1.d = 0;
+            }
+            else{
+                t += pr2[j].bt;
+                update(remaining, arrived, pr2, t);
+                done.insert(j);
+                g1.d = 1;
+            }
+
+            // cout << "t:" << t << endl;
+            // update(remaining, arrived, pr2, t);
+            g1.endt = t;
+            g.push_back(g1);
+            // cout << pr2[j].id << endl;
+        }
+    }
+    cout<<endl;
+    cout<<"******** Gantt Chart: ********\n";
+    // cout<<"\tTOA\t"<<"Process"<<endl;
+    cout<<"--------------------------------------------------------------------------------------------------------------------------------\n";
+    cout<<"|";
+    for(struct gantt s : g){
+        cout<<"  P" << s.prid<<"  |";
+    }
+    cout<<endl;
+    cout<<"--------------------------------------------------------------------------------------------------------------------------------\n";
+    cout<<"\nProcess\tTAT\tWT\n";
+    int totalTAT = 0; 
+    int totalWT = 0;
+    for(struct gantt s: g){
+        if(s.d == 1){
+        int tat = s.endt - pr[s.prid].at;
+        int wt = tat - pr[s.prid].bt;
+        cout<<"  P"<<s.prid<<"\t"<< tat <<"\t"<< wt <<endl;
+        totalTAT += tat;
+        totalWT += wt;
+        }
+    }
+    cout<<"\nAvg TAT = "<<totalTAT/10.0<<"\t"<<"Avg WT = "<<totalWT/10.0;
+    cout<<"\n<--------------------END-------------------->\n\n";
+    return 0;
+}
